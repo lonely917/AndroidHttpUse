433325a0c0d8891a47f909e5ae10d4fc60919515
wenburgyan <wenboyan2014@163.com>
2018-11-18 22:32:31 +0800

    init project

---
+++ app/src/test/java/http/wenburgyan/top/androidhttpuse/YanTest.java
@@ -0,0 +1,11 @@
+package http.wenburgyan.top.androidhttpuse;
+
+import org.junit.Test;
+
+public class YanTest {
+
+    @Test
+    public void testOne(){
+        System.out.println("testOne");
+    }
+}
